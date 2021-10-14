1cbfb46c0bbba2b52aaa8affdc37d0068366bd04
Eileen Xue <xue.eileen@gmail.com>
2021-10-14 16:36:10 +0000

    Update files to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// Teachers that assisted during a certain cohort
-pool.query(`
+// Values
+const queryString = 
+`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(*) AS total_assistances
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 GROUP BY cohorts.name, teachers.name
 ORDER BY teacher;
-`)
+`;
+const cohortName = [`%${process.argv[2]}%`];
+
+// Teachers that assisted during a certain cohort
+pool.query(queryString, cohortName)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
